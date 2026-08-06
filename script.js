65c349e684c932599e9ce76281c36db13f06ee37
oneshot5722 <Rawhaangowhar@gmail.com>
2026-08-06 22:00:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -120,5 +120,15 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
     }
+/* ===========================
+   MOUSE SPOTLIGHT
+=========================== */
 
+document.addEventListener("mousemove",(e)=>{
+
+document.body.style.setProperty("--mouse-x",e.clientX+"px");
+
+document.body.style.setProperty("--mouse-y",e.clientY+"px");
+
+});
 });
