b4836235acd652825a6869afef0add8f624b538a
oneshot5722 <Rawhaangowhar@gmail.com>
2026-08-14 14:28:33 +0530

    Add Supabase configuration file

---
+++ supabase-config.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://vmsjhbzoxokyyorbaunx.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_thBBM8ug2IHUKSp7jIz-3g_5hdanJms";
+
+window.HEISTSIXIN_SUPABASE = {
+    url: SUPABASE_URL,
+    key: SUPABASE_ANON_KEY
+};
